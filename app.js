b7d02ff7dad6103ae32de969c747ff56d9106ce2
Amr11matarek <amr11matarek@gmail.com>
2024-10-30 13:15:59 +0300

    Routes

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
-    console.log(`Listening on port ${PORT}`);
+const patients = require("./Routes/patient");
+const doctors = require("./Routes/doctor");
+const labs = require("./Routes/labs");
+const clinc = require("./Routes/clinc");
+const chats = require("./Routes/chat");
+const doctorAuth = require("./Routes/doctorAuth");
+const patientAuth = require("./Routes/patientAuth");
+const appointments = require("./Routes/appointment");
+// const chatBot = require('./Routes/chatBot');
+// const rating = require('./Routes/rate');
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
