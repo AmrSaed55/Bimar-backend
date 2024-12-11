32300484dc902cc40ce19a14066548e7394ba708
Abdulrhman Mohammed <110139732+3bdalrahman@users.noreply.github.com>
2024-12-11 02:25:17 +0200

    Create ecosystem.js

---
+++ ecosystem.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+      {
+        name: "bimar-backend",
+        script: "app.js",
+        env: {
+          NODE_ENV: "production",
+          PORT: 3000
+        }
+      }
+    ]
+  };
+  
