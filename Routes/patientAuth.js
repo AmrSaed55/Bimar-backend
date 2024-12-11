460eea79526e3cdeb48b3c87676998ccf0a1bfb6
Abdulrhman Mohammed <110139732+3bdalrahman@users.noreply.github.com>
2024-12-11 17:29:01 +0200

    git ignore

--- Routes/patientAuth.js
+++ Routes/patientAuth.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const PatinetAuthController = require ('./../controllers/PatientAuth')
-const uservalidator = require('./../validation/patientAuthValid')
+const patientvalidator = require('./../validation/patientAuthValid')
 
-router.post('/patientRegister' ,uservalidator.userValidation(), PatinetAuthController.register);
+router.post('/patientRegister' ,patientvalidator.userValidation(), PatinetAuthController.register);
 router.post('/patientLogin',PatinetAuthController.login);
 router.post('/forgot-password', PatinetAuthController.forgetpassword);
-router.post('/verify-otp',PatinetAuthController.verifyotp)
+router.post('/verify-otp',PatinetAuthController.verifyotp);
 router.post('/reset-password', PatinetAuthController.resetPassword);
 
 module.exports = router
