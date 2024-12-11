a1d62171864ebce7603768733e842cd430eebc88
Abdulrhman Mohammed <110139732+3bdalrahman@users.noreply.github.com>
2024-12-11 02:53:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const app = express();
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config()
-const cookieParse = require('cookie-parser');
-//for executing the webhook
-const exec = require('child_process');
+const cookieParse = require('cookie-parser')
+
 
 const patients = require("./Routes/patient");
 const doctors = require("./Routes/doctor");
@@ -28,30 +27,12 @@ mongoose.connect(process.env.DB).then(()=>{
   console.log('DB Connected')
 })
 
-// for applying automatic pull after each push
-app.post('/webhook', (req, res) => {
-  console.log('Webhook received!');
-  exec('git -C C:\\Users\\Administrator\\Desktop\\Bimar\\Bimar-backend pull origin main', (err, stdout, stderr) => {
-    if (err) {
-      console.error('Error pulling from GitHub:', stderr);
-      res.status(500).send('Error pulling from GitHub');
-      return;
-    }
-    console.log('Git pull successful:', stdout);
-    res.status(200).send('Git pull successful');
-  });
-});
-
 app.listen(process.env.port||3000, () => {
   console.log(`Listening on port ${process.env.port}`);
 });
-
-
 
 
 app.use(express.json())
 app.use(cookieParse())
 app.use('/patientsAuth',patientAuth)
 app.use('/medical-records',medicalRecordRoutes)
-
-
