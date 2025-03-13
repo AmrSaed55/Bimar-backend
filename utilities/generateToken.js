dd6b26dc18bf49a492aa100329724aa206296bb7
Kareem Abdallah <kareemabdallah789@outlook.com>
2025-03-13 16:58:19 +0200

    Update generateToken.js

--- utilities/generateToken.js
+++ utilities/generateToken.js
@@ -7,8 +7,7 @@ const generateTokenAndSetCookie = (userId, res) => {
 
   // set cokies
   res.cookie("jwt", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000, 
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    maxAge: 15 * 24 * 60 * 60 * 1000,
     sameSite: "strict", // CSRF attacks cross-site request forgery attacks
     secure: process.env.NODE_ENV !== "development",
   });
