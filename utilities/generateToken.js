b2efcdcdd2e90083e98ba9e893a27cea8f22973f
Kareem Abdallah <kareemabdallah789@outlook.com>
2025-04-15 11:06:17 +0200

    solve the token problem

--- utilities/generateToken.js
+++ utilities/generateToken.js
@@ -4,11 +4,7 @@ const generateTokenAndSetCookie = (userId, role, res) => {
   const token = jwt.sign({ userId, role }, process.env.JWT_KEY);
 
   // Set cookie with matching expiration
-  res.cookie("jwt", token, {
-    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
-  });
+  res.cookie("jwt", token);
 };
 
 export default generateTokenAndSetCookie;
